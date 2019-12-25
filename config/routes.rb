2223c6f2c2eeadd26ab89008917338e671d144c3
cabechi <houkb1@icloud.com>
2019-12-25 23:12:18 +0900

    第２章完了？

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'users#index'
   resources :users
+  resources :posts
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
